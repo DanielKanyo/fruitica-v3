e200f95d51a2c967601297bfe670b3f6ea3e5514
DEV-XKDA <brueckner.de\dev-xkda@RH-DEV-XKDA.brueckner.de>
2026-02-20 13:26:46 +0100

    Css fix and README update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Mantine Vite template
+# Fruitica
 
-Get started with the template by clicking `Use this template` button on the top of the page.
-
-[Documentation](https://mantine.dev/guides/vite/)
+This repository contains the source code for the official website of Fruitica d.o.o., a company based in Chantavir specializing in the import and distribution of industrial products for the food and beverage industry.
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -41,7 +41,13 @@ function Home() {
 							Products
 						</Button>
 
-						<Button size="lg" radius="xl" variant="white" color="grey">
+						<Button
+							style={{
+								border: "1px solid rgba(255, 255, 255, 0.2)",
+								background: "rgba(255, 255, 255, 0.1)",
+								backdropFilter: "blur(10px)",
+							}} size="lg" radius="xl" variant="outline" color="white"
+						>
 							Contact
 						</Button>
 					</Group>
