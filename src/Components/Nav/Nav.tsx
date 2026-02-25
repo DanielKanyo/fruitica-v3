bab1193ed513989cf35054c6ce23ae6bf7bfdde9
DEV-XKDA <brueckner.de\dev-xkda@RH-DEV-XKDA.brueckner.de>
2026-02-25 13:45:49 +0100

    Fade in animation for Nav and Home

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,8 +1,8 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 
-import { ActionIcon, Button, Group, Menu, Tooltip, Paper, useMantineTheme, Center, Burger, Drawer } from "@mantine/core";
+import { ActionIcon, Button, Group, Menu, Tooltip, Paper, useMantineTheme, Center, Burger, Drawer, Transition } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconLanguage, IconLemon } from "@tabler/icons-react";
 
@@ -16,6 +16,7 @@ function Nav() {
 	const theme = useMantineTheme();
 	const { t } = useTranslation();
 	const [opened, { open, close }] = useDisclosure(false);
+	const [mounted, setMounted] = useState(false);
 
 	const items = [
 		{ label: "About", link: "about" },
@@ -24,6 +25,10 @@ function Nav() {
 		{ label: "Contact", link: "contact" },
 	];
 
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
 	const handleLanguageSelect = useCallback((key: LanguagesKeys) => {
 		i18n.changeLanguage(key);
 		setLanguage(key);
@@ -31,62 +36,76 @@ function Nav() {
 
 	return (
 		<>
-			<Center>
-				<div className="nav-bar-container">
-					<Paper className="transparent-element" p="md">
-						<Group justify="space-between">
-							<NavLink to="/" style={{ height: 44 }} className={({ isActive }) => (isActive ? "nav-active" : "")}>
-								<ActionIcon
-									className="nav-button"
-									variant="transparent"
-									size="xl"
-									color="white"
-									radius="xl"
-									aria-label="Home"
-								>
-									<IconLemon size={32} />
-								</ActionIcon>
-							</NavLink>
-
-							<Burger className="burger" color="white" opened={opened} onClick={open} aria-label="Toggle drawer" />
-
-							<Group className="nav-items" justify="center" gap="sm">
-								{items.map((item) => (
-									<NavLink key={item.label} to={item.link} className={({ isActive }) => (isActive ? "nav-active" : "")}>
-										<Button className="nav-button" variant="transparent" size="md" radius="xl">
-											{item.label}
-										</Button>
+			<Transition mounted={mounted} transition="fade-down" duration={500} timingFunction="ease">
+				{(styles) => (
+					<Center style={styles}>
+						<div className="nav-bar-container">
+							<Paper className="transparent-element" p="md">
+								<Group justify="space-between">
+									<NavLink to="/" style={{ height: 44 }} className={({ isActive }) => (isActive ? "nav-active" : "")}>
+										<ActionIcon
+											className="nav-button"
+											variant="transparent"
+											size="xl"
+											color="white"
+											radius="xl"
+											aria-label="Home"
+										>
+											<IconLemon size={32} />
+										</ActionIcon>
 									</NavLink>
-								))}
 
-								<Menu shadow="md" width={110} withArrow radius="lg">
-									<Menu.Target>
-										<Tooltip label={t("language")} withArrow>
-											<ActionIcon variant="transparent" size="xl" color="white" radius="xl" aria-label="Language">
-												<IconLanguage size={28} />
-											</ActionIcon>
-										</Tooltip>
-									</Menu.Target>
+									<Burger className="burger" color="white" opened={opened} onClick={open} aria-label="Toggle drawer" />
 
-									<Menu.Dropdown>
-										<Menu.Label>{t("language")}</Menu.Label>
-										{Languages.map((lng) => (
-											<Menu.Item
-												key={lng.code}
-												ta="center"
-												c={language === lng.code ? theme.colors.red[8] : "dark"}
-												onClick={() => handleLanguageSelect(lng.code)}
+									<Group className="nav-items" justify="center" gap="sm">
+										{items.map((item) => (
+											<NavLink
+												key={item.label}
+												to={item.link}
+												className={({ isActive }) => (isActive ? "nav-active" : "")}
 											>
-												{lng.name}
-											</Menu.Item>
+												<Button className="nav-button" variant="transparent" size="md" radius="xl">
+													{item.label}
+												</Button>
+											</NavLink>
 										))}
-									</Menu.Dropdown>
-								</Menu>
-							</Group>
-						</Group>
-					</Paper>
-				</div>
-			</Center>
+
+										<Menu shadow="md" width={110} withArrow radius="lg">
+											<Menu.Target>
+												<Tooltip label={t("language")} withArrow>
+													<ActionIcon
+														variant="transparent"
+														size="xl"
+														color="white"
+														radius="xl"
+														aria-label="Language"
+													>
+														<IconLanguage size={28} />
+													</ActionIcon>
+												</Tooltip>
+											</Menu.Target>
+
+											<Menu.Dropdown>
+												<Menu.Label>{t("language")}</Menu.Label>
+												{Languages.map((lng) => (
+													<Menu.Item
+														key={lng.code}
+														ta="center"
+														c={language === lng.code ? theme.colors.red[8] : "dark"}
+														onClick={() => handleLanguageSelect(lng.code)}
+													>
+														{lng.name}
+													</Menu.Item>
+												))}
+											</Menu.Dropdown>
+										</Menu>
+									</Group>
+								</Group>
+							</Paper>
+						</div>
+					</Center>
+				)}
+			</Transition>
 
 			<Drawer
 				offset={8}
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import { useMantineTheme, Stack, Text, Button, Group, Center } from "@mantine/core";
+import { useMantineTheme, Stack, Text, Button, Group, Center, Transition, Flex } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 
 import logo from "../../Assets/Images/fruitica-logo.png";
@@ -9,34 +10,51 @@ function Home() {
 	const theme = useMantineTheme();
 	const { t } = useTranslation();
 	const isMobile = useMediaQuery("(max-width: 1000px)");
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
 	return (
 		<div style={{ height: "100vh" }}>
 			<Center style={{ height: "100vh", zIndex: 10 }}>
 				<Stack align="center" gap="md">
-					<img src={logo} alt="Fruitica Logo" width={isMobile ? 330 : 880} style={{ marginBottom: 18 }} />
-					<Text
-						style={{
-							fontSize: isMobile ? 18 : 22,
-							fontWeight: 400,
-							color: theme.colors.gray[1],
-							maxWidth: 450,
-							textAlign: "center",
-							position: "relative",
-						}}
-					>
-						{t("subtitle")}
-					</Text>
+					<Transition mounted={mounted} transition="fade-up" duration={500} enterDelay={500} timingFunction="ease">
+						{(styles) => (
+							<Flex direction="column" align="center" style={styles}>
+								<img src={logo} alt="Fruitica Logo" width={isMobile ? 330 : 880} style={{ marginBottom: 18 }} />
+								<Text
+									style={{
+										fontSize: isMobile ? 18 : 22,
+										fontWeight: 400,
+										color: theme.colors.gray[1],
+										maxWidth: 450,
+										textAlign: "center",
+										position: "relative",
+									}}
+								>
+									{t("subtitle")}
+								</Text>
 
-					<Group mt="md">
-						<Button size={isMobile ? "md" : "lg"} radius="xl" variant="filled" color="red.8">
-							Products
-						</Button>
+								<Group mt="md">
+									<Button size={isMobile ? "md" : "lg"} radius="xl" variant="filled" color="red.8">
+										Products
+									</Button>
 
-						<Button className="transparent-element" size={isMobile ? "md" : "lg"} radius="xl" variant="outline" color="white">
-							Contact
-						</Button>
-					</Group>
+									<Button
+										className="transparent-element"
+										size={isMobile ? "md" : "lg"}
+										radius="xl"
+										variant="outline"
+										color="white"
+									>
+										Contact
+									</Button>
+								</Group>
+							</Flex>
+						)}
+					</Transition>
 				</Stack>
 			</Center>
 		</div>
