d1173ba6d031080f3b4ff281067b78ec3dec53fd
DEV-XKDA <brueckner.de\dev-xkda@RH-DEV-XKDA.brueckner.de>
2026-03-05 13:45:56 +0100

    Brown background removed from other section

--- src/Pages/Home/Sections/OtherSection.tsx
+++ src/Pages/Home/Sections/OtherSection.tsx
@@ -42,159 +42,153 @@ function OtherSection({ isMobile }: OtherSectionProps) {
 	const autoplay = useRef(Autoplay({ delay: 1500000 }));
 
 	return (
-		<div
-			style={{
-				background: "linear-gradient(90deg, #3f251d 0%, #2c1913 100%)",
-			}}
+		<Carousel
+			withIndicators
+			bg="transparent"
+			style={{ zIndex: 10, marginTop: -160 }}
+			withControls={false}
+			emblaOptions={{ dragFree: true, align: "start" }}
+			plugins={[autoplay.current]}
+			classNames={{ indicator: "carousel-indicator" }}
 		>
-			<Carousel
-				withIndicators
-				bg="transparent"
-				style={{ zIndex: 10, marginTop: -160 }}
-				withControls={false}
-				emblaOptions={{ dragFree: true, align: "start" }}
-				plugins={[autoplay.current]}
-				classNames={{ indicator: "carousel-indicator" }}
-			>
-				<Carousel.Slide>
-					<Flex gap="xl" style={{ padding: isMobile ? "4rem 1rem" : "4rem 6rem" }} direction={{ base: "column", lg: "row" }}>
-						<Card bg="transparent" p={0} w="100%" pos="relative" shadow="xs" radius="xl">
-							<div className="card-bg coffee-bg"></div>
-							<Box className="card-header">
-								<Text c="white" fz={22}>
-									Our Competitive <br /> <b>Advantage</b>
-								</Text>
-							</Box>
+			<Carousel.Slide>
+				<Flex gap="xl" style={{ padding: isMobile ? "4rem 1rem" : "4rem 6rem" }} direction={{ base: "column", lg: "row" }}>
+					<Card bg="transparent" p={0} w="100%" pos="relative" shadow="xs" radius="xl">
+						<div className="card-bg coffee-bg"></div>
+						<Box className="card-header">
+							<Text c="white" fz={22}>
+								Our Competitive <br /> <b>Advantage</b>
+							</Text>
+						</Box>
 
-							<Card
-								mt={-40}
-								style={{
-									borderTopLeftRadius: "var(--mantine-radius-xl)",
-									borderTopRightRadius: "var(--mantine-radius-xl)",
-									borderBottomLeftRadius: 0,
-									borderBottomRightRadius: 0,
-									height: "100%",
-								}}
-								className="card-body"
-							>
-								<Stack w="90%" gap="xl" p="xl">
-									<Text c="gray.4" fz={isMobile ? 17 : 18}>
-										Unlike traditional traders, we:
-									</Text>
-									<List
-										spacing="md"
-										fw={400}
-										icon={
-											<ThemeIcon color="#523228" size={24} radius="xl">
-												<IconCheck size={16} />
-											</ThemeIcon>
-										}
-									>
-										{COMPETITIVE_LIST_ITEMS.map((item) => (
-											<List.Item key={item} fz={isMobile ? 17 : 18} c="gray.4">
-												{item}
-											</List.Item>
-										))}
-									</List>
-									<Text c="gray.4" fz={isMobile ? 17 : 18}>
-										Fruitica d.o.o combines commercial strength with technological understanding.
-									</Text>
-								</Stack>
-							</Card>
-						</Card>
-						<Card bg="transparent" p={0} w="100%" pos="relative" shadow="xs" radius="xl">
-							<div className="card-bg blueberries-bg"></div>
-							<Box className="card-header">
-								<Text c="white" fz={22}>
-									Private Label & Industrial <br /> <b>Solutions</b>
+						<Card
+							mt={-40}
+							style={{
+								borderTopLeftRadius: "var(--mantine-radius-xl)",
+								borderTopRightRadius: "var(--mantine-radius-xl)",
+								borderBottomLeftRadius: 0,
+								borderBottomRightRadius: 0,
+								height: "100%",
+							}}
+							className="card-body"
+						>
+							<Stack w="90%" gap="xl" p="xl">
+								<Text c="gray.4" fz={isMobile ? 17 : 18}>
+									Unlike traditional traders, we:
 								</Text>
-							</Box>
+								<List
+									spacing="md"
+									fw={400}
+									icon={
+										<ThemeIcon color="#523228" size={24} radius="xl">
+											<IconCheck size={16} />
+										</ThemeIcon>
+									}
+								>
+									{COMPETITIVE_LIST_ITEMS.map((item) => (
+										<List.Item key={item} fz={isMobile ? 17 : 18} c="gray.4">
+											{item}
+										</List.Item>
+									))}
+								</List>
+								<Text c="gray.4" fz={isMobile ? 17 : 18}>
+									Fruitica d.o.o combines commercial strength with technological understanding.
+								</Text>
+							</Stack>
+						</Card>
+					</Card>
+					<Card bg="transparent" p={0} w="100%" pos="relative" shadow="xs" radius="xl">
+						<div className="card-bg blueberries-bg"></div>
+						<Box className="card-header">
+							<Text c="white" fz={22}>
+								Private Label & Industrial <br /> <b>Solutions</b>
+							</Text>
+						</Box>
 
-							<Card
-								mt={-40}
-								style={{
-									borderTopLeftRadius: "var(--mantine-radius-xl)",
-									borderTopRightRadius: "var(--mantine-radius-xl)",
-									borderBottomLeftRadius: 0,
-									borderBottomRightRadius: 0,
-									height: "100%",
-								}}
-								className="card-body"
-							>
-								<Stack w="90%" gap="xl" p="xl">
-									<Text c="gray.4" fz={isMobile ? 17 : 18}>
-										Fruitica d.o.o is a trusted private label partner for retail chains and distributions.
-										<br />
-										We provide full-service development:
-									</Text>
-									<List
-										spacing="md"
-										fw={400}
-										icon={
-											<ThemeIcon color="blue.8" size={24} radius="xl">
-												<IconCheck size={16} />
-											</ThemeIcon>
-										}
-									>
-										{INDUSTRIAL_SOLUTION_LIST_ITEMS.map((item) => (
-											<List.Item key={item} fz={isMobile ? 17 : 18} c="gray.4">
-												{item}
-											</List.Item>
-										))}
-									</List>
-								</Stack>
-							</Card>
-						</Card>
-						<Card bg="transparent" p={0} w="100%" pos="relative" shadow="xs" radius="xl">
-							<div className="card-bg rasberries-bg"></div>
-							<Box className="card-header">
-								<Text c="white" fz={22}>
-									Export & <br /> <b>Distribution</b>
+						<Card
+							mt={-40}
+							style={{
+								borderTopLeftRadius: "var(--mantine-radius-xl)",
+								borderTopRightRadius: "var(--mantine-radius-xl)",
+								borderBottomLeftRadius: 0,
+								borderBottomRightRadius: 0,
+								height: "100%",
+							}}
+							className="card-body"
+						>
+							<Stack w="90%" gap="xl" p="xl">
+								<Text c="gray.4" fz={isMobile ? 17 : 18}>
+									Fruitica d.o.o is a trusted private label partner for retail chains and distributions.
+									<br />
+									We provide full-service development:
 								</Text>
-							</Box>
-							<Card
-								mt={-40}
-								style={{
-									borderTopLeftRadius: "var(--mantine-radius-xl)",
-									borderTopRightRadius: "var(--mantine-radius-xl)",
-									borderBottomLeftRadius: 0,
-									borderBottomRightRadius: 0,
-									height: "100%",
-								}}
-								className="card-body"
-							>
-								<Stack w="90%" gap="xl" p="xl">
-									<Text c="gray.4" fz={isMobile ? 17 : 18}>
-										We cooperate with partners across Europe and other international markets.
-										<br />
-										Advantages:
-									</Text>
-									<List
-										spacing="md"
-										fw={400}
-										icon={
-											<ThemeIcon color="pink.9" size={24} radius="xl">
-												<IconCheck size={16} />
-											</ThemeIcon>
-										}
-									>
-										{EXPORT_ADVANTAGES_LIST_ITEMS.map((item) => (
-											<List.Item key={item} fz={isMobile ? 17 : 18} c="gray.4">
-												{item}
-											</List.Item>
-										))}
-									</List>
-									<Text c="gray.4" fz={isMobile ? 17 : 18}>
-										We are continuously seeking long-term distribution partnerships.
-									</Text>
-								</Stack>
-							</Card>
+								<List
+									spacing="md"
+									fw={400}
+									icon={
+										<ThemeIcon color="blue.8" size={24} radius="xl">
+											<IconCheck size={16} />
+										</ThemeIcon>
+									}
+								>
+									{INDUSTRIAL_SOLUTION_LIST_ITEMS.map((item) => (
+										<List.Item key={item} fz={isMobile ? 17 : 18} c="gray.4">
+											{item}
+										</List.Item>
+									))}
+								</List>
+							</Stack>
 						</Card>
-					</Flex>
-				</Carousel.Slide>
-				<Carousel.Slide>2</Carousel.Slide>
-			</Carousel>
-		</div>
+					</Card>
+					<Card bg="transparent" p={0} w="100%" pos="relative" shadow="xs" radius="xl">
+						<div className="card-bg rasberries-bg"></div>
+						<Box className="card-header">
+							<Text c="white" fz={22}>
+								Export & <br /> <b>Distribution</b>
+							</Text>
+						</Box>
+						<Card
+							mt={-40}
+							style={{
+								borderTopLeftRadius: "var(--mantine-radius-xl)",
+								borderTopRightRadius: "var(--mantine-radius-xl)",
+								borderBottomLeftRadius: 0,
+								borderBottomRightRadius: 0,
+								height: "100%",
+							}}
+							className="card-body"
+						>
+							<Stack w="90%" gap="xl" p="xl">
+								<Text c="gray.4" fz={isMobile ? 17 : 18}>
+									We cooperate with partners across Europe and other international markets.
+									<br />
+									Advantages:
+								</Text>
+								<List
+									spacing="md"
+									fw={400}
+									icon={
+										<ThemeIcon color="pink.9" size={24} radius="xl">
+											<IconCheck size={16} />
+										</ThemeIcon>
+									}
+								>
+									{EXPORT_ADVANTAGES_LIST_ITEMS.map((item) => (
+										<List.Item key={item} fz={isMobile ? 17 : 18} c="gray.4">
+											{item}
+										</List.Item>
+									))}
+								</List>
+								<Text c="gray.4" fz={isMobile ? 17 : 18}>
+									We are continuously seeking long-term distribution partnerships.
+								</Text>
+							</Stack>
+						</Card>
+					</Card>
+				</Flex>
+			</Carousel.Slide>
+			<Carousel.Slide>2</Carousel.Slide>
+		</Carousel>
 	);
 }
 
