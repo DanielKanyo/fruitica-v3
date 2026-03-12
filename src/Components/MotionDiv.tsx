21b9f9a944d0d2386eccc88eb9a3e91f9b3378e5
DEV-XKDA <brueckner.de\dev-xkda@RH-DEV-XKDA.brueckner.de>
2026-03-12 14:47:20 +0100

    Viewport amount value fix

--- src/Components/MotionDiv.tsx
+++ src/Components/MotionDiv.tsx
@@ -12,7 +12,7 @@ function MotionDiv({ children }: MotionDivProps) {
 			initial={{ opacity: 0, y: 20 }}
 			whileInView={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5, ease: "easeOut" }}
-			viewport={{ once: true, amount: 0.3 }}
+			viewport={{ once: true, amount: 0.2 }}
 		>
 			{children}
 		</motion.div>
